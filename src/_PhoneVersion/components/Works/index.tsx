ed5a8420abb580b72e81ec37bf4bc33bb41f299d
Pierre Portal <pierreportal.mac@gmail.com>
2022-07-01 15:06:14 +0200

    test

--- src/_PhoneVersion/components/Works/index.tsx
+++ src/_PhoneVersion/components/Works/index.tsx
@@ -2,25 +2,18 @@ import React from 'react';
 
 
 export const Works:React.FunctionComponent = () => {
-	const [t, setT] = React.useState<any>('jbk');
+	const [aungmentedMode, setAugmentedMode] = React.useState<boolean>(false);
 
-	const testMoving = (event:any) => {
-			setT('moving');
-	}
-
-	
 	const handleClick = () => {
-
-
 	if (typeof (DeviceMotionEvent as any).requestPermission === 'function') {
 		(DeviceMotionEvent as any).requestPermission().then((permissionState: any) => {
-			setT(permissionState); 
 			if (permissionState === 'granted') {
+				setAugmentedMode(true); 
 			// DeviceMotionEvent.requestPermission() has been granted
 			}
 			}).catch(console.error);
 		}
 	}
 	React.useEffect(handleClick,[]);
-	return <button >Works {t}</button>;
+	return <button >Enter</button>;
 }
