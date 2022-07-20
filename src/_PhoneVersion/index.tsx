c45f06d20dcda731ef2fd7372325203efcd30811
Pierre Portal <pierreportal.mac@gmail.com>
2022-07-20 09:39:45 +0200

    fix page style

--- src/_PhoneVersion/index.tsx
+++ src/_PhoneVersion/index.tsx
@@ -1,32 +1,28 @@
-import React from 'react';
-import { Works} from './components/Works';
-import { PhoneAppMainContainer } from './styles';
+import React from "react";
+import { Works } from "./components/Works";
+import "./mobile-style.css"
+import { PhoneAppMainContainer } from "./styles";
 
 (() => {
-
 	const log = console.log.bind(console);
 	const error = console.error.bind(console);
 	console.log = (...args) => {
-		log(...args)
-		//var tab = window.open('about:blank', '_blank');
-		//tab?.document.write(`<h1>Logs</h1>${args.map((c:string) => `<p><b>${c}<b/></p>`).join('')}`); // where 'html' is a variable containing your HTML
-		//tab?.document.close(); // to finish loading the page
-	}
+		log(...args);
+	};
 	console.error = (...args) => {
-		error(...args)
-		//var tab = window.open('about:blank', '_blank');
-		//tab?.document.write(`<h1>Logs</h1>${args.map((c:string) => `<p><b>${c}<b/></p>`).join('')}`); // where 'html' is a variable containing your HTML
-		//tab?.document.close(); // to finish loading the page
-
-	}
+		error(...args);
+	};
 })();
 
-export const PhoneVersion:React.FunctionComponent = () => {
+export const PhoneVersion: React.FunctionComponent = () => {
 
-	return <PhoneAppMainContainer>
-		<h1>
-			AR Phone Version in construction...
-		</h1>
-					<Works/>
-	</PhoneAppMainContainer>
-}
+	return (
+		<PhoneAppMainContainer>
+			<div id="content">
+				<h1>AR Phone Version in construction...</h1>
+				<p>Check out the desktop version and have a look at <a href="https://www.github.com/pierreportal">my work here</a></p>
+			</div>
+			<Works />
+		</PhoneAppMainContainer>
+	);
+};
--- src/_PhoneVersion/styles.tsx
+++ src/_PhoneVersion/styles.tsx
@@ -1,14 +1,19 @@
 import styled from 'styled-components';
 
 export const PhoneAppMainContainer = styled.div`
+
 	alignItems: center;
 	display: flex;
 	flexDirection: column;
 	width: 100vw;
 	height: 100vh;
-	overflow: hidden;
 	color: #eff2f4;
 	backgroundColor: #2d2d44;
+
+	& .a-enter-vr {
+		display: none;
+	}
+
 
 	& > button {
 		fontSize: 1.5rem;
@@ -17,16 +22,4 @@ export const PhoneAppMainContainer = styled.div`
 		position: absolute;
 	}
 
-	& > h1 {
-		marginTop: 20px;
-		fontSize: 1.5em;
-		height: 100px;
-		width: 200px;
-		textAlign: center;
-		position: absolute;
-		display: flex;
-		alignSelf: center;
-		{/* position: absolute; */}
-		color: red;
-	}
 `;
